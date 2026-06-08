5e7905825ff0464dddecd5766bc864aca9102abd
a7madrefa3y <ahmadrefai2@gmail.com>
2026-06-08 13:11:27 +0200

    added math round to the vars

--- src/components/cards/DailyForecast.tsx
+++ src/components/cards/DailyForecast.tsx
@@ -15,11 +15,19 @@ export default function DailyForecast({}: Props) {
         <div className="flex flex-col gap-4">
           {data?.daily.map((day) => (
             <div key={day.dt} className="flex justify-between">
-              <p>DATE</p>
+              <p>
+                {new Date(day.dt * 1000).toLocaleDateString(undefined, {
+                  weekday: "short",
+                })}
+              </p>
               <img
+                className="size-8"
                 src={`https://openweathermap.org/payload/api/media/file/${day.weather[0].icon}.png`}
                 alt="weather icon"
               />
+              <p> {Math.round(day.temp.day)}</p>
+              <p className="text-gray-500/75"> {Math.round(day.temp.min)}</p>
+              <p className="text-gray-500/75"> {Math.round(day.temp.max)}</p>
             </div>
           ))}
         </div>
