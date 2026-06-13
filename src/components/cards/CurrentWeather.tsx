3e55dfa2b0a4f80e7df6fef3906b68e0a2f2a710
a7madrefa3y <ahmadrefai2@gmail.com>
2026-06-13 23:45:43 +0200

    added the current weather configs

--- src/components/cards/CurrentWeather.tsx
+++ src/components/cards/CurrentWeather.tsx
@@ -1,4 +1,5 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
+import WeatherIcon from "./WeatherIcon.tsx";
 import Card from "./Card.tsx";
 import { getWeather } from "../../api.ts";
 
@@ -9,5 +10,45 @@ export default function CurrentWeather({}: Props) {
     queryKey: ["weather"],
     queryFn: () => getWeather({ lat: 35.6895, lon: 139.6917 }),
   });
-  return <Card title="CurrentWeather"> CurrentWeather</Card>;
+  return (
+    <Card
+      title="CurrentWeather"
+      childrenClassName="flex flex-col items-center gap-6"
+    >
+      <div className="flex flex-col gap-2 items-center">
+        <h3 className="text-6xl font-semibold text-center">
+          {Math.round(data.current.temp)}°C
+        </h3>
+        <WeatherIcon src={data.current.weather[0].icon} className="size-14" />
+        <h3 className="capitalize text-xl">
+          {data.current.weather[0].description}
+        </h3>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-xl text-center">Local Time: </p>
+        <h3 className="text-4xl font-semibold">
+          {new Intl.DateTimeFormat("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+            timeZone: data.timezone,
+          }).format(new Date(data.current.dt * 1000))}
+        </h3>
+      </div>
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-gray-500">Feels Like</p>
+          <p>{Math.round(data.current.feels_like)}°C</p>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-gray-500">Humidity</p>
+          <p>{data.current.humidity}%</p>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-gray-500">Wind</p>
+          <p>{data.current.wind_speed}km/h</p>
+        </div>
+      </div>
+    </Card>
+  );
 }
