745a4f32d371a5e756a08888c6b5cd47e15d35cd
a7madrefa3y <ahmadrefai2@gmail.com>
2026-06-13 17:35:25 +0200

    adjust the time for the hourlyforcast

--- src/components/cards/HourlyForecast.tsx
+++ src/components/cards/HourlyForecast.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { getWeather } from "../../api.ts";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import Card from "./Card";
@@ -18,8 +17,14 @@ export default function HourlyForecast({}: Props) {
       childrenClassName="flex gap-6 overflow-x-scroll"
     >
       {data.hourly.map((hour) => (
-        <div className="flex flex-col gap-2">
-          <p>{new Date(hour.dt * 1000).toLocaleTimeString()}</p>
+        <div className="flex flex-col gap-2 items-center">
+          <p className="whitespace-nowrap">
+            {new Date(hour.dt * 1000).toLocaleTimeString(undefined, {
+              hour: "numeric",
+              minute: "2-digit",
+              hour12: false,
+            })}
+          </p>
           <WeatherIcon src={hour.weather[0].icon} />
           <p>{Math.round(hour.temp)}°C</p>
         </div>
