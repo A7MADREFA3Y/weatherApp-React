3fc7e7711dfdd35bba377da18d5395ef914695fe
a7madrefa3y <ahmadrefai2@gmail.com>
2026-06-19 20:53:37 +0200

    create new comp. Map.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import Aditionsallinfo from "./components/cards/Aditionsallinfo.tsx";
 import CurrentWeather from "./components/cards/CurrentWeather.tsx";
 import DailyForecast from "./components/cards/DailyForecast.tsx";
 import HourlyForecast from "./components/cards/HourlyForecast.tsx";
+import Map from "./components/Map.tsx";
 
 function App() {
   return (
     <div className="flex flex-col gap-8">
-      <Aditionsallinfo />
+      <Map></Map>
       <CurrentWeather />
       <HourlyForecast />
       <DailyForecast />
+      <Aditionsallinfo />
     </div>
   );
 }
